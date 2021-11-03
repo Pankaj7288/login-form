a49fe60a5ae75ea50c2b84fbc88eb8046be41202
Pankaj Bepari <beparipankaj@gmail.com>
2021-11-03 23:20:43 +0600

    Login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BsKey } from "react-icons/bs";
+import {
+  FacebookLoginButton,
+  GoogleLoginButton
+} from "react-social-login-buttons";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form className="w-100 h-100 p-4 mx-auto my-3 mw-350 border border-1 border-secondary rounded-3">
+        <h1 className="fw-bold">MyWebSite.com</h1>
+        <h2 className="text-center">Welcome!</h2>
+        <FormGroup>
+          <Label>Email:</Label>
+          <Input type="email" placeholder="Email" />
+          <Label>Password:</Label>
+          <Input type="password" placeholder="Password" />
+        </FormGroup>
+        <div className="d-grid gap-2">
+          <Button className="btn-lg" color="primary">
+            <BsKey /> Login
+          </Button>
+          <span className="text-center fw-bold">
+            Continue with your social account
+          </span>
+          <FacebookLoginButton />
+          <GoogleLoginButton />
+        </div>
+        <div className="text-center">
+          <a href="/signup">Sign Up</a>
+          <span> | </span>
+          <a href="/signup">Forgot Password</a>
+        </div>
+      </Form>
     </div>
   );
 }
